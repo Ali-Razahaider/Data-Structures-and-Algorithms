8b6de419ee293598c292521604c98acea99e583b
Ali <alirazahaider2@gmail.com>
2024-10-11 23:33:25 +0500

    Tuples Studied basics

--- tuples/tuples.cpp
+++ tuples/tuples.cpp
@@ -4,5 +4,13 @@
 using namespace std;
 
 int main(){
-    cout << "Hello world";
+    tuple<int, string> car(2004,"BMW M5 CS with twin turbo V8");
+
+
+    tuple<string> intro;
+    intro = make_tuple("Introducing the new ");
+    cout << get<0>(intro) << get<1>(car) << endl;
+
+    auto sentence = tuple_cat(intro, car);
+    cout  << get<0>(intro) << get<1>(car) << endl;
 }
